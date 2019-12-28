faf59e735fe0b89e23ed574d8759192b40bff1a3
SergeyGulin <gulin.sergey1@mail.ru>
2019-12-28 12:54:34 +0300

    init

---
+++ src/App/Sounds.ts
@@ -0,0 +1,6 @@
+export const sound1 = new Audio(
+  process.env.PUBLIC_URL + "/sounds/162485__kastenfrosch__space.mp3"
+);
+export const sound2 = new Audio(
+  process.env.PUBLIC_URL + "/sounds/135510__chriddof__space-bloop.wav"
+);
