a1a882f58199dc6fa7c778e49c2a2d57e991cbc9
Vyacheslav Tuev <vyacheslavtuev@192.168.0.100>
2019-12-30 22:29:14 +0300

    Updates

---
+++ precache-manifest.6734dea1375c73981ddaa459f4269c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283697f33e0219744a32c859cb3b9197",
+    "url": "/chem/index.html"
+  },
+  {
+    "revision": "f4b6056db77dbed6644e",
+    "url": "/chem/static/css/main.9463b28e.chunk.css"
+  },
+  {
+    "revision": "d359367da1956544d1ba",
+    "url": "/chem/static/js/2.d512b2c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chem/static/js/2.d512b2c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4b6056db77dbed6644e",
+    "url": "/chem/static/js/main.aa03cc16.chunk.js"
+  },
+  {
+    "revision": "44902319f365fcaa3088",
+    "url": "/chem/static/js/runtime-main.98e10cb5.js"
+  }
+]);
--- src/App/Sounds.ts
+++
@@ -1,6 +0,0 @@
-export const sound1 = new Audio(
-  process.env.PUBLIC_URL + "/sounds/162485__kastenfrosch__space.mp3"
-);
-export const sound2 = new Audio(
-  process.env.PUBLIC_URL + "/sounds/135510__chriddof__space-bloop.wav"
-);
