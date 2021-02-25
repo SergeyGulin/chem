faf56e2bcda64e70ae0017df4a98b2251e33fe31
Viacheslav Tuev <vvtuev@gmail.com>
2021-02-25 10:04:09 +0300

    fix style

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -170,7 +170,7 @@ const App: React.FC = () => {
                     {GRADE_CLASS_ARRAY.map((grade, index) => (
                         <Button
                             key={grade}
-                            name={`Начать игру "${grade}"`}
+                            name={`"${grade}"`}
                             className={
                                 animationStep
                                     ? `buttonPositionPlay${index + 1} transition-true`
